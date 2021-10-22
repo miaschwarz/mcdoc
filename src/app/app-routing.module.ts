6f93a85a9f596ac508b649025b4dae438618f700
Mia Schwarz <miaschwarz@mias-mbp.lan>
2021-10-22 13:38:16 +0100

    final of goal section, more details of sketches, added header to all pages, deleted home and instead refreshed to GOAL, started user stories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
 import { GoalComponent } from './goal/goal.component';
 import { SketchesComponent } from './sketches/sketches.component';
 import { UserstoriesComponent } from './userstories/userstories.component';
@@ -9,10 +8,6 @@ import { TechnologiesComponent } from './technologies/technologies.component';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
   {
     path: 'goal',
     component: GoalComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GoalComponent } from './goal/goal.component';
@@ -16,7 +15,6 @@ import { TechnologiesComponent } from './technologies/technologies.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     GoalComponent,
     SketchesComponent,
