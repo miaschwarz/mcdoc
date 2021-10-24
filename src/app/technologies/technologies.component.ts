e873081ca7904714daa88483564b2497c4937361
Mia Schwarz <miaschwarz@mias-mbp.lan>
2021-10-24 14:42:57 +0100

    finishing touches of technologies w/ ng boostrap detail

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+import {NgbCollapse} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-technologies',
   templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.scss']
+  styleUrls: ['./technologies.component.scss'],
 })
 export class TechnologiesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit(): void {
   }
 
 }
+
